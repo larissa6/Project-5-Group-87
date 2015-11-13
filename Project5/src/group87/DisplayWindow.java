4b56bc31f86a635521dbd947b214fe312d952839
larissa6 <larissa6@vt.edu>
2015-11-13 18:19:30 -0500

    fixed error in display window

--- Project5/src/group87/DisplayWindow.java
+++ Project5/src/group87/DisplayWindow.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD:Project5/src/group87/DisplayWindow.java
 package group87;
 
 public class DisplayWindow
 {
 
 }
-=======
-package group87;
-
-public class DisplayWindow
-{
-
-}
->>>>>>> origin/master:Project5/src/group87/DisplayWindow.java
